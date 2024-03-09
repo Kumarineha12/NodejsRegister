9600cdcac935c8019d6479ed0ab96362fbf2230d
Neha <Neha@codewithNeha.com>
2024-03-09 18:33:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const path=require("path");
+const mongoose=require("mongoose");
+const cookieParser=require("cookie-parser");
+const jwt= require("jsonwebtoken");
+const bcrypt=require("bcrypt");
+mongoose.connect("mongodb://127.0.0.1:27017",{
+  dbName:"Backend",
+}).then(()=>
+  console.log("Database Connected"))
+  .catch((e)=>console.log(e));
+const userSchema= new mongoose.Schema({
+    name:String,
+    email: String,
+    password: String,
+  });
+const User=mongoose.model("User", userSchema)
+const app=express();
+//using middleware
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+//setting up view engine
+
+app.set("view engine", "ejs");
+
+const isAuthenticated= async (req,res,next)=>{
+  const {token} =req.cookies;
+  if(token){
+    // res.render("logout");
+  const decoded= jwt.verify(token, "ababababab");
+   req.user= await User.findById(decoded._id);  
+  
+  next();
+  }else{
+    res.redirect("/login");
+  }
+};
+
+app.get("/",isAuthenticated, (req,res)=>{
+  // console.log(req.cookies.token);
+  // console.log(req.user);
+ res.render("logout");
+});
+
+
+app.get("/register", (req,res)=>{
+
+ res.render("register");
+});
+
+//add data into database
+// app.get("/add", async (req,res)=>{
+//   await Messge.create({name: "Sachin", email:"s@gmail.com"});
+//     res.send("Nice");
+// })
+app.get("/login",(req,res)=>{
+  res.render("login");
+} );
+
+app.post("/login", async(req,res)=>{
+  const {email, password}=req.body;
+  let user=await User.findOne({email});
+  if(!user) return res.redirect("/register")
+  
+ //MATCH HASH CODEDED PASSWORD WITH PAWWORD WHICH YOU WRITE IN LOGIN
+
+
+  // const isMatch= user.password===password;
+  const isMatch= await bcrypt.compare(password, user.password);
+
+  if(!isMatch) return res.render("login",{email,message: "Incorrect Password"});
+  const token =jwt.sign({_id:user._id}, "ababababab");
+  res.cookie("token", token,{
+    httpOnly:true,
+    expires:new Date(Date.now()+60*1000),
+  });
+  res.redirect("/");
+});
+
+app.post("/register",async (req,res)=>{
+ const{name,email,password}=req.body;
+
+ let user=await User.findOne({email})
+ if(user){
+  //  return console.log("Register First")
+return res.redirect("/login"); 
+}
+
+  const hashedPassword=await bcrypt.hash(password,10);
+
+
+  user=await User.create({
+   name,
+   email,
+   password: hashedPassword,
+});
+
+ const token =jwt.sign({_id:user._id}, "ababababab");
+  res.cookie("token", token,{
+    httpOnly:true,
+    expires:new Date(Date.now()+60*1000),
+  });
+  res.redirect("/");
+})
+
+app.get("/logout", (req,res)=>{
+  res.cookie("token", null,{
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/");
+});
+
+
+
+app.listen(500, ()=>{
+    console.log("server is working");
+})
